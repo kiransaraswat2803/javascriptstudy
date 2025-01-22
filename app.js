25e184719471f4ccd54fc0866db1bacec881dea5
kiran <kiransaraswat2803@gmail.com>
2025-01-22 21:29:48 +0530

    new chnages

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-function sum(a,b)
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+// console.log(sum(2,3));
+function greet(hello,name)
 {
-    return a+b;
+   return hello+name;
 }
-console.log(sum(2,3));
+console(greet("hi","kiran"));
+
+class User{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    greet()
+    {
+      console.log("kiranji");
+    }
+    
+}
+const user1=new User("sandeep",28);
+console.log(user1);
