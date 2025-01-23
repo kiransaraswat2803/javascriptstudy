631d2ff4e459226d76d1da446844f544ca73aa0f
kiran <kiransaraswat2803@gmail.com>
2025-01-23 20:55:25 +0530

    add changes

--- app.js
+++ app.js
@@ -3,22 +3,34 @@
 //     return a+b;
 // }
 // console.log(sum(2,3));
-function greet(hello,name)
-{
-   return hello+name;
-}
-console(greet("hi","kiran"));
+// function greet(hello,name)
+// {
+//    return hello+name;
+// }
+// console.log(greet("hi","kiran"));
 
-class User{
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-    }
-    greet()
-    {
-      console.log("kiranji");
-    }
+// class User{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+//     greet()
+//     {
+//       console.log("kiranji");
+//     }
     
-}
-const user1=new User("sandeep",28);
-console.log(user1);
+// }
+// const user1=new User("sandeep",28);
+// console.log(user1);
+//arrays
+// const names=["kiran","sandeep"]
+// console.log(names[0]);
+
+const sports=(["vollyball","basketball"])
+
+    console.log(sports[1]);
+    
+
+const index=sports.push("cricket");
+const index1=sports.indexOf((t)=>t=="cricket");
+console.log(index);
