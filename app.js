7045bac862c62758f4ba0021dd6131cec73ebb5f
kiransaraswat <kiran.saraswat@mediologysoftware.com>
2025-01-23 10:00:58 +0530

    add files

--- app.js
+++ app.js
@@ -3,22 +3,44 @@
 //     return a+b;
 // }
 // console.log(sum(2,3));
-function greet(hello,name)
-{
-   return hello+name;
-}
-console(greet("hi","kiran"));
+// function greet(hello,name)
+// {
+//    return hello+name;
+// }
+// console(greet("hi","kiran"));
 
-class User{
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-    }
-    greet()
-    {
-      console.log("kiranji");
-    }
+// class User{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+//     greet()
+//     {
+//       console.log("kiranji");
+//     }
     
+// }
+//  const user1=new User("sandeep",28);
+// console.log(user1);
+const array=[1,2,3,4,5];
+{
+  console.log(array);
 }
-const user1=new User("sandeep",28);
-console.log(user1);
+const array1=[1,2,3,4,5];
+{
+ 
+ console.log(array1);
+}
+const array2=[...array,...array1];
+{
+  console.log(array2);
+}
+const names={
+  name:"kiran",
+  sirname:"saraswat"
+}
+const hobbies=[{first:"reading"},{second:"writing"}];
+const user={...names,...hobbies};
+{
+  console.log(user);
+}
