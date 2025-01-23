73d9d2f5109c9ea42e079f460339b20197843999
kiransaraswat <kiran.saraswat@mediologysoftware.com>
2025-01-23 16:48:16 +0530

    changes made

--- javascriptstudyy/app1.js
+++ javascriptstudyy/app1.js
@@ -59,42 +59,62 @@
 // }
 // console.log(reverses("kiran"));
  
-function countVowels(vowel) {
+// function countVowels(vowel) {
     
-    if (vowel.includes("a") || vowel.includes("e") || vowel.includes("i") || vowel.includes("o") || vowel.includes("u"))
-        {
-        return ("true");
-        } 
-    else
-    { 
-        return ("false");
-    } 
-}
-console.log(countVowels("kiran"));
-function vowles(name)
-{
-    let count=0;
-    let vowels="aeiou";
-    for(let i=0;i<name.length;i++)
-    {
-        if(vowels.includes(name[i]))
-        {
-            count++;
-        }
-    }
-    return count;
-}
-console.log(vowles("kiran"));
-// Output: 2
- function hek(nhi){
+//     if (vowel.includes("a") || vowel.includes("e") || vowel.includes("i") || vowel.includes("o") || vowel.includes("u"))
+//         {
+//         return ("true");
+//         } 
+//     else
+//     { 
+//         return ("false");
+//     } 
+// }
+// console.log(countVowels("kiran"));
+// function vowles(name)
+// {
+//     let count=0;
+//     let vowels="aeiou";
+//     for(let i=0;i<name.length;i++)
+//     {
+//         if(vowels.includes(name[i]))
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(vowles("kiran"));
+// // Output: 2
+//  function hek(nhi){
     
-    {
-        const nhii= nhi.split("").reverse().join("");
-        return nhi.reverse==reverse.nhii;
-    }
+//     {
+//         const nhii= nhi.split("").reverse().join("");
+//         return nhi.reverse==reverse.nhii;
+//     }
    
- }
-    console.log(hek("naman"));
-    
+//  }
+//     console.log(hek("naman"));
+
+// // Output: true
+// console.log(hek("kiran"));
+
+// // Output: false
+// function fibonaci(n){
+//     let a=0,b=1,next;
+
+//     for(i=0;i<n;i++)
+//     {
+//         next=a+b;
+//         a=b;
+//         b=next;
+//         console.log(b);
+//     }
+// }
+// console.log(fibonaci(3));
+// Output: 1 2 3
+// Output: 1 2 3 5 8 13 21 34 55 89
+// Output: 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
+
 
 
